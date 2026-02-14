267716c94cb60a19b0ffc6b0c796600042d57a94
Arosha Jayathilaka <acjayathilake@gamil.com>
2026-02-14 07:58:41 +0530

    CORS added

--- backend/ApiGateway/Program.cs
+++ backend/ApiGateway/Program.cs
@@ -3,11 +3,30 @@ using Ocelot.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Configuration.AddJsonFile("ocelot.json", optional: false);
+builder.Configuration.AddJsonFile(
+    "ocelot.json",
+    optional: false,
+    reloadOnChange: true
+);
 
-builder.Services.AddOcelot();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("GatewayCors", policy =>
+    {
+        policy
+            .WithOrigins("https://black-cliff-081510d0f.1.azurestaticapps.net") 
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+    });
+});
+
+builder.Services.AddOcelot(builder.Configuration);
 
 var app = builder.Build();
+
+app.UseCors("GatewayCors");
+
+app.UseHttpsRedirection();
 
 await app.UseOcelot();
 
